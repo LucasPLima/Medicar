1df494e114cfceecd0375828d010425b54737309
Lucas Pereira <lucas.plima.ti@gmail.com>
2021-05-09 15:28:23 -0300

    Alteração de dados retornados no endpoint de agenda. Só são retornadas as agendas com data maior do que a data atual.

--- backend/agenda/views.py
+++ backend/agenda/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Agenda
 from .serializers import AgendaSerializer
+from datetime import date
 
 # Create your views here.
 class AgendaViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Agenda.objects.all().order_by('dia')
+    queryset = Agenda.objects.filter(dia__gte=date.today()).order_by('dia')
     serializer_class = AgendaSerializer
