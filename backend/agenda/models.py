e8a9f6909dbf8b8dc46a65dab65ad69a15a9c3ed
Lucas Pereira <lucas.plima.ti@gmail.com>
2021-05-09 13:07:41 -0300

    Inclusão de métodos "str" nos models criados.

--- backend/agenda/models.py
+++ backend/agenda/models.py
@@ -7,7 +7,13 @@ class Agenda(models.Model):
     medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
     dia = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
 
+    def __str__(self):
+        return f"Agenda {self.id} - {self.dia} | Médico: {self.medico}"
+
 class Horario(models.Model):
     agenda = models.ForeignKey(Agenda, related_name='horarios', on_delete=models.CASCADE)
     hora = models.TimeField(auto_now=False, auto_now_add=False)
     marcado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Horario: {self.hora} - ({self.agenda.dia} - Médico: {self.agenda.medico.nome} | CRM: {self.agenda.medico.crm})"
--- backend/medico/models.py
+++ backend/medico/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Especialidade(models.Model):
     nome = models.CharField(max_length=40, unique=True)
 
+    def __str__(self):
+        return self.nome
+
 class Medico(models.Model):
     nome = models.CharField(max_length= 150)
     crm = models.IntegerField(unique=True)
@@ -11,3 +14,6 @@ class Medico(models.Model):
     telefone = models.CharField(max_length= 13, null=True, blank=True)
     especialidade = models.ForeignKey(Especialidade, 
                                       on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.nome} - CRM: {self.crm} - {self.especialidade}'
