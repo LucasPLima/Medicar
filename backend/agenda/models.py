b77ebe8f8edce5ee38c0dae7e35d953cdc3a81e9
Lucas Pereira <lucas.plima.ti@gmail.com>
2021-05-09 18:17:16 -0300

    Inclusão de validações no cadastro da agenda e horarios. - Não é possível criar mais de uma agenda para o médico na mesma dataç - Não é possível inserir o mesmo horário duas vezes em uma agenda.

--- backend/agenda/models.py
+++ backend/agenda/models.py
@@ -1,8 +1,10 @@
 from django.db import models
-from medico.models import Medico
 from django.utils import timezone
+
+from medico.models import Medico
 from medicar import settings
-from datetime import datetime
+from datetime import datetime, date
+from django.core.exceptions import ValidationError
 
 def format_date(date):
     return date.strftime('%d-%m-%Y')
@@ -13,11 +15,35 @@ class Agenda(models.Model):
 
     def __str__(self):
         return f"Agenda {self.id} - {format_date(self.dia)} | Médico: {self.medico}"
+    
+    def clean(self):
+        def validate_data():
+            if self.dia < date.today():
+                raise ValidationError({'dia':'Data informada deve ser maior que data atual.'})
+        
+        def validate_agenda_medico():
+            try:
+                agenda = Agenda.objects.filter(medico=self.medico, dia=self.dia).get()
+                raise ValidationError({'medico':'Médico já contém uma agenda criada nessa data.'})
+            except Agenda.DoesNotExist:
+                pass
+
+        validate_data()
+        validate_agenda_medico()
 
 class Horario(models.Model):
     agenda = models.ForeignKey(Agenda, related_name='horarios', on_delete=models.CASCADE)
     hora = models.TimeField(auto_now=False, auto_now_add=False)
     marcado = models.BooleanField(default=False)
+
+    def clean(self):
+        def validate_horario_value():
+            try:
+                horario = Horario.objects.filter(agenda=self.agenda, hora=self.hora).get()
+                raise ValidationError({'hora':'Agenda já contém hora informada.'})
+            except Horario.DoesNotExist:
+                pass
+        validate_horario_value()
 
     def __str__(self):
         return f"Horario: {self.hora} - ({format_date(self.agenda.dia)} - Médico: {self.agenda.medico.nome} | CRM: {self.agenda.medico.crm})"
