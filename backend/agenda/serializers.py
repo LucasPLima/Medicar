322afb3b8cc33abf9512dd6834d22f67eb82a2ad
Lucas Pereira <lucas.plima.ti@gmail.com>
2021-05-10 22:23:16 -0300

    Refatoração do serializer da agenda.

--- backend/agenda/serializers.py
+++ backend/agenda/serializers.py
@@ -1,17 +1,18 @@
-from agenda.models import Agenda, Horario
+from agenda.models import Agenda
 from rest_framework import serializers
 from medico.serializers import MedicoSerializer
 from medicar import settings
 
 class AgendaSerializer(serializers.ModelSerializer):
     medico = MedicoSerializer()
+    dia = serializers.DateField(format='%d/%m/%Y', input_formats=settings.DATE_INPUT_FORMATS)
     horarios = serializers.SlugRelatedField(
         many=True,
         read_only=True,
         slug_field='hora'
     )
-    dia = serializers.DateField(format='%d/%m/%Y', input_formats=settings.DATE_INPUT_FORMATS)
-    #horarios = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    
+    
     class Meta:
         model = Agenda
         fields = ['id','medico', 'dia', 'horarios']
