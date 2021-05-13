09ad466d8b523ecfd10278cca3d4d46d35f268f7
Lucas Pereira <lucas.plima.ti@gmail.com>
2021-05-13 03:11:16 -0300

    Inclusão de filtros de pesquisa. -Filtro de busca na especialidade e nome de médico.

--- backend/medico/views.py
+++ backend/medico/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework import viewsets
 from .models import Especialidade, Medico
 from .serializers import MedicoSerializer, EspecialidadeSerializer
@@ -8,8 +9,14 @@ from rest_framework.permissions import IsAuthenticated
 class EspecialidadeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Especialidade.objects.all()
     serializer_class = EspecialidadeSerializer
-
+    permission_classes =[IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome']
 class MedicoViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Medico.objects.all()
     serializer_class = MedicoSerializer
     permission_classes =[IsAuthenticated]
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['nome', ]
+    filterset_fields = ['especialidade']
+    
