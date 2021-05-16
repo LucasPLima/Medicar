e73b2d3b541fbc76c63ddfb7e1aa5db4bf15e83e
Lucas Pereira <lucas.plima.ti@gmail.com>
2021-05-16 14:08:11 -0300

    Inclusão de informações recebidas no cadastro. - Inclusão de nome e sobrenome.

--- backend/usuario/serializers.py
+++ backend/usuario/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields =['username', 'email', 'password']
+        fields =['first_name', 'last_name','username', 'email', 'password']
         extra_kwargs ={
             'password':{'write_only': 'password'}
         }
@@ -23,6 +23,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def save(self,):
         new_user = User(username=self.validated_data['username'], 
                         email=self.validated_data['email'],
+                        first_name=self.validated_data['first_name'],
+                        last_name=self.validated_data['last_name'],
                         )
         new_user.set_password(self.validated_data['password'])
         new_user.save()
