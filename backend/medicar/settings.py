68c2ff6e902dea1c4613de73d5dec75050c8fda8
Lucas Pereira <lucas.plima.ti@gmail.com>
2021-05-16 15:39:41 -0300

    Ajustes de configuração settings.py.

--- backend/medicar/settings.py
+++ backend/medicar/settings.py
@@ -16,8 +16,7 @@ import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -143,3 +142,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
