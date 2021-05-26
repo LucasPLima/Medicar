c3b2484582cfeb2a35da54f7e6ec30b4767af796
Lucas P <lucas.plima.ti@gmail.com>
2021-05-26 00:03:57 -0300

    Adição de filtro por médico na busca de agendas.

--- backend/agenda/views.py
+++ backend/agenda/views.py
@@ -1,11 +1,10 @@
 from rest_framework import viewsets
 from django.db.models import Prefetch, OuterRef, Exists
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Agenda, Horario
 from .serializers import AgendaSerializer
-from django.utils import timezone
-from datetime import date
 # Create your views here.
 class AgendaViewSet(viewsets.ReadOnlyModelViewSet):
     horarios_desmarcados = Horario.objects.disponiveis().filter(agenda=OuterRef('pk'), marcado=False)
@@ -17,3 +16,5 @@ class AgendaViewSet(viewsets.ReadOnlyModelViewSet):
                                                 ).filter(Exists(horarios_desmarcados))
     serializer_class = AgendaSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['medico']
