50c52eb34ce75a7634b7003e5a5dbcf56a62e6fc
Lucas P <lucas.plima.ti@gmail.com>
2021-05-29 13:18:56 -0300

    Melhorias funcionais na administração de médicos, agendas e horários. - Adição de list_displays e list_filters ao superusuário.

--- backend/agenda/admin.py
+++ backend/agenda/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Agenda, Horario
 
+class AgendaAdmin(admin.ModelAdmin):
+    list_display=('dia','medico')
+    list_filter=('dia','medico')
+
+class HorarioAdmin(admin.ModelAdmin):
+    list_display=('agenda','hora','marcado')
+    list_filter=('agenda',)
+
 # Register your models here.
-admin.site.register(Horario)
-admin.site.register(Agenda)
+admin.site.register(Horario, HorarioAdmin)
+admin.site.register(Agenda, AgendaAdmin)
--- backend/medico/admin.py
+++ backend/medico/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Medico, Especialidade
-# Register your models here.
 
+class MedicoAdmin(admin.ModelAdmin):
+    list_display=('nome','crm','email','especialidade')
+    list_filter=('especialidade',)
+
+# Register your models here.
 admin.site.register(Especialidade)
-admin.site.register(Medico)
+admin.site.register(Medico, MedicoAdmin)
